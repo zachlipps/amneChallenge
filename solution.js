324d89f16df01d931cbbbdda9b747d5576f09d27
Zach Lipps <zacharylipps10@gmail.com>
2017-08-30 16:40:47 -0700

    parse needed variables

--- solution.js
+++ solution.js
@@ -4,8 +4,8 @@ fs.readFile('input.txt', function(err, data) {
   if (err) throw err;
 
   const array = data.toString().split("\n");
+  const k = Number(array[0].split(' ')[1]);
+  const housePrices = array[1].split(' ');
+  const n = housePrices.length;
 
-  for (const item in array) {
-    console.log(array[item]);
-  }
 });
