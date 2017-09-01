bb7840ab4b3c55a1c89f0f8d31a96adcc5b0c4c7
Zach Lipps <zacharylipps10@gmail.com>
2017-09-01 12:53:28 -0700

    fix bug where long runs not added to queue before need to decrement them

--- solution.js
+++ solution.js
@@ -3,29 +3,32 @@ const fs = require('fs');
 fs.readFile('input.txt', (err, data) => {
   if (err) throw err;
 
-  const array = data.toString().split('\n');
+  const array = data.toString().trim().split('\n');
   const k = Number(array[0].split(' ')[1]);
   const housePrices = array[1].split(' ');
   const n = housePrices.length;
 
   let currentSubrangeTotal = 0;
-  let currentRun;
+  let currentRun = 1;
   let runDirection;
   let result = '';
+  let currentQueueIndex = 0;
   const runQueue = [];
 
-
-  const enqueueRun = () => {
-    if (runDirection) {
-      runQueue.push({
-        direction: runDirection,
-        magnitude: currentRun,
-      });
-    }
+  
+  const addToRun = () => {
+    runQueue[currentQueueIndex] = {
+      direction: runDirection,
+      magnitude: currentRun,
+    };
   };
-
+  
   const dequeueRun = () => {
     const { direction, magnitude } = runQueue[0];
+
+    if (currentQueueIndex === 0) {
+      currentRun -= 1;
+    }
 
     if (direction === 'inc') {
       currentSubrangeTotal -= magnitude;
@@ -36,47 +39,46 @@ fs.readFile('input.txt', (err, data) => {
     } else {
       runQueue[0].magnitude -= 1;
     }
-
+    
     if (runQueue[0].magnitude === 0) {
       runQueue.shift();
+      currentQueueIndex -= 1;
     }
   };
-
+  
   const findTrend = (index) => {
     const currentPrice = housePrices[index];
     const previousPrice = housePrices[index - 1];
-  
+    
     if (currentPrice > previousPrice) {
       if (runDirection === 'inc') {
         currentRun += 1;
-      } else {
-        enqueueRun();
+      } else if (runDirection) {
         currentRun = 1;
+        currentQueueIndex += 1;
       }
       runDirection = 'inc';
+      addToRun();
       currentSubrangeTotal += currentRun;
     } else if (currentPrice < previousPrice) {
       if (runDirection === 'dec') {
         currentRun += 1;
-      } else {
-        enqueueRun();
+      } else if (runDirection) {
         currentRun = 1;
+        currentQueueIndex += 1;
       }
       runDirection = 'dec';
+      addToRun();
       currentSubrangeTotal -= currentRun;
     } else if (currentPrice === previousPrice) {
       if (runDirection === 'flat') {
         currentRun += 1;
-      } else {
-        enqueueRun();
+      } else if (runDirection) {
         currentRun = 1;
+        currentQueueIndex += 1;
       }
       runDirection = 'flat';
-    }
-
-    // this is unnecesary beacuse dont need to queue last element as it can never be dequeued
-    if (index === n - 1) {
-      enqueueRun();
+      addToRun();
     }
   };
 
@@ -92,15 +94,14 @@ fs.readFile('input.txt', (err, data) => {
       // need to sum first window and queue all of the "runs"
       findTrend(i);
     } else {
-      // write current answer to outout file
       result += `${currentSubrangeTotal}\n`;
-      // find trend at next index
       findTrend(i);
-      // decrement the first run
       dequeueRun();
     }
   }
+
   // add this for last output which is found after last trend is found
   result += `${currentSubrangeTotal}`;
   writeOutput(result);
+  console.log(runQueue);
 });
